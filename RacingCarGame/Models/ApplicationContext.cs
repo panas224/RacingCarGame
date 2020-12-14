31e305da00421324dacc3657bf1afdf55f4e56b2
panas224 <panas.oleh2019@gmail.com>
2020-12-14 20:37:37 +0200

    working with DB

---
+++ RacingCarGame/Models/ApplicationContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//namespace RacingCarGame.Models
+//{
+    //class ApplicationContext:DbContext
+    //{
+    ////    public DbSet<User> Users { get; set; }
+    ////    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    ////    {
+    ////        optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=relationsdb;Trusted_Connection=True;");
+    ////    }
+    ////}
+//}
